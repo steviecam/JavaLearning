9722e1312fc1fd6079cf4d68ed167b5bc38e31f9
stephencampbell <stevecam@sky.com>
2017-08-03 15:04:52 +0100

    Update to code txt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -13,6 +15,7 @@ public class Main {
             System.out.println("Even number " + number);
             number++;
             System.out.println("Heeeloooo");
+            System.out.println("GtHubTest");
         }
         //isAnEvenNumber();
 
